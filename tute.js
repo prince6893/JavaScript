b3aa1d79bc16683346fc41de477a92e40354e5ce
Prince Kumar <princefreak40@gmail.com>
2018-08-15 01:47:15 +0530

    Tute

---
+++ tute.js
@@ -0,0 +1,16 @@
+// example one
+const playerOneName = "tim"
+const playerTwoName = "jenn"
+const playerOneMarker = "X"
+const playerTwoMarker = "O"
+
+// example two
+const playerOne = {
+  name: "tim",
+  marker: "X"
+}
+
+const playerTwo = {
+  name: "jenn",
+  marker: "O"
+}
